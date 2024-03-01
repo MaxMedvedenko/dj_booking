1c794a6ede2ac6d91a6b72ac7cad86f666ca12e3
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-03-01 18:04:20 +0200

    changes

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from booking_app.models import *
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'booking_app/index.html')
+    room = Room.objects.all()
+    content = {'rooms': room}
+    return render(request, 'booking_app/index.html', content)
+
+def get_room_by_id(request, room_id):
+    try:
+        # Спробувати отримати кімнату за її id
+        room = Room.objects.get(pk=room_id)
+        
+        # Якщо кімната знайдена, вивести її інформацію
+        response = f"Назва кімнати: {room.name}, Тип кімнати: {room.room_type}"
+        
+        return HttpResponse(response)
+    except Room.DoesNotExist:
+        # Якщо кімната не знайдена, повернути пусту відповідь або іншу обробку за необхідності
+        return HttpResponse("Кімната з вказаним ідентифікатором не знайдена")
